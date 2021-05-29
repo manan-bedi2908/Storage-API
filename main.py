8e55334917ef545814272fd46a4bcb4022e30b30
manan-bedi2908 <manan.19535@sscbs.du.ac.in>
2021-05-30 01:27:18 +0530

    add color extraction

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from flask import Flask
+from PIL import Image
+
 app = Flask(__name__)
+
+def find_dominant_color(filename):
+    #Resizing parameters
+    width, height = 150,150
+    image = Image.open(filename)
+    image = image.resize((width, height),resample = 0)
+    #Get colors from image object
+    pixels = image.getcolors(width * height)
+    #Sort them by count number(first element of tuple)
+    sorted_pixels = sorted(pixels, key=lambda t: t[0])
+    #Get the most frequent color
+    dominant_color = sorted_pixels[-1][1]
+    return dominant_color
+
+filename = r"C:\Users\WINDOWS 10\Desktop\hackCBS4.0-responsive\assets\images\first.png"
+color = find_dominant_color(filename)
+print(color)
 
 @app.route('/')
 def home():
-    return 1;
+    return "<h1>Home Page</h1>"
 
-@app.route('/image', methods=['GET','POST'])
-def get_src():
-    src =
-
-
-@app.route('/shubhangi.com')
-def data():
-    return "hello"
-
-if __name__ == '__main__':
+if __name__ == 'main':
     app.run(debug=True)
